8ee9199d6c4ed78c71b3eeb3a1d688561c5148c2
Essien Saviour <31137610+iamessien@users.noreply.github.com>
2018-03-19 15:25:29 +0100

    Add files via upload
    This is files

---
+++ Agro-Vine/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Open Farm</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="img/agrovine.png" id="logo" alt="Open Farm Logo">
+            </div>
+            <div class="col-md-3">
+                11, Elekahia Road, Port Harcourt
+                <br>
+                Find agents near you
+            </div>
+            <div class="col-md-3">
+                info@openfarm.com.ng
+                <br>
+                +23407034696973
+            </div>
+            <div class="col-md-3">
+                <button class="btn btn-success">Logout</button>
+            </div>
+        </div>
+    </div>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Funding</a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Training</a>
+      </li>
+         <li class="nav-item">
+        <a class="nav-link" href="#">News</a>
+      </li>
+         <li class="nav-item">
+        <a class="nav-link" href="#">Storage Facilities</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    
+    
+    
+</body>
+    
+</html>
